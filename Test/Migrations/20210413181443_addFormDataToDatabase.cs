fa84456715d7925b82393f364971cb5de81a6708
KPEKZ <ed_010@mail.ru>
2021-04-13 21:53:20 +0300

    etr

---
+++ Test/Migrations/20210413181443_addFormDataToDatabase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Test.Migrations
+{
+    public partial class addFormDataToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FormData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    personName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    personEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    personText = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+               name: "FormData");
+        }
+    }
+}
---
+++ Test/Models/FormData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Test.Models
+{
+    public class FormData
+    {
+        [Key]
+        string userName { get; set; }
+        string userEmail { get; set; }
+        string userText { get; set; }
+    }
+}
